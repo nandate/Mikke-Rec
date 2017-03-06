cfe537de971a3a3bd77344e3e2ad3d5831f82f5e
nandate <o238049t@mail.kyutech.jp>
2017-03-06 19:13:29 +0900

    add firebase

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var app = express();
+var firebase = require('firebase');
 
-app.get('/',function(req,res){
-
-  res.send('Hello World');
+firebase.initializeApp({
+  serviceAccount: './fire-test-6ce685d5b105.json',
+  databaseURL: 'https://fire-test-4a04c.firebaseio.com/'
 });
 
-var server = app.listen(process.env.PORT,process.env.IP);
+var db = firebase.database();
+var ref = db.ref("genres");
+
+
+app.get('/',function(req,res){
+  res.send('Hello World');
+  ref.once("value",function(snapshot){
+    console.log(snapshot.val());
+  })
+});
+
+
+
+var server = app.listen(3000);
+//var server = app.listen(process.env.PORT,process.env.IP);
